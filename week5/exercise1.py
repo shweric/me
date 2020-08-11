cd24f5e5e6a77a1e017a9f3ebeefc1e5f0dd9eb9
Shing Hei Wong (Eric) <z5280200@unsw.edu.au>
2020-08-11 04:03:48 -0400

    traingle master

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -163,12 +163,16 @@ def tell_me_about_this_right_triangle(facts_dictionary):
 
 
 def triangle_master(base, height, return_diagram=False, return_dictionary=False):
+    dictionary_of_triangle_facts = get_triangle_facts(base, height)
+
+    diagram = tell_me_about_this_right_triangle(dictionary_of_triangle_facts)
+
     if return_diagram and return_dictionary:
-        return None
+        return {"diagram": diagram, "dictionary": dictionary_of_triangle_facts}
     elif return_diagram:
-        return None
+        return diagram
     elif return_dictionary:
-        return None
+        return dictionary_of_triangle_facts
     else:
         print("You're an odd one, you don't want anything!")
 
